f7406c565a91986bd47967937cd5b6ccb0d18043
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-07-05 17:31:53 -0500

    js refresher

---
+++ js/101.js
@@ -0,0 +1,313 @@
+"use strict"; // leave this line here :)
+
+// Keep this function definition in order to test expected vs. actual results
+const assert = function(actual, expected, message = "") {
+
+    // This compares primitive values and collections. If they differ, throw an error.
+    if(JSON.stringify(actual) !== JSON.stringify(expected)) {
+        console.error('[assert] expected:    ' + expected)
+        console.error('[assert] instead got: ' + actual)
+        throw Error("Assert failed in " + message);
+    }
+};
+
+
+// Keep this function here in order to add correct questions to the counter
+function addToDone(message) {
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(message);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    node.classList.add("finished");
+    document.querySelector(".correct ul").appendChild(node);
+    document.querySelector("#count").innerHTML = document.querySelectorAll('.finished').length + " of 101";
+}
+
+
+// Welcome to 101 Exercises in JS
+// If you get "ReferenceError: someVariableName is not defined", that means you need to create a variable
+
+// Exercise #0, Example Problem:
+// Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
+// The line below creates the variable named doingJSRightNow and assigns the boolean value true
+// To complete Exercise #0, uncomment the following line of JS
+// var doingJSRightNow = true
+
+// The lines below will test your answer. If you see an error, then it means that your answer is incorrect or incomplete.
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Not_defined to understand this error message.
+var doingJSRightNow = true;
+
+assert(doingJSRightNow, true, "Exercise 0");
+
+
+//  Exercise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+// For more on variables, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables
+
+var onMarsRightNow = false;
+
+assert(onMarsRightNow, false, "Exercise 1");
+addToDone("Exercise 1 is correct.");
+
+// Exercise 2
+// For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+
+var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"];
+
+assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry"], "Exercise 2");
+addToDone("Exercise 2 is correct.");
+
+// Exercise 3
+// Create a variable named vegetables and assign it an array of strings containing the following vegetables.
+// eggplant, broccoli, carrot, cauliflower, and zucchini
+
+var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
+
+assert(vegetables, ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"], "Exercise 3");
+addToDone("Exercise 3 is correct.");
+
+
+// Exercise 4
+// Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+assert(numbers, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Exercise 4");
+addToDone("Exercise 4 is correct.");
+
+
+// Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+// *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
+
+fruits.push("tomato");
+
+assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"], "Exercise 5");
+addToDone("Exercise 5 is correct");
+
+// Exercise 6
+// add the string "tomato" onto the end of the vegetables array.
+// Recommend using the built-in JS operation to add to an array.
+
+vegetables.push("tomato");
+
+
+assert(vegetables,["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"], "Exercise 6");
+addToDone("Exercise 6 is correct")
+
+
+// Exercise 7
+// Given the array of numbers defined below, reverse the array of numbers that you created above.
+var someNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+someNumbers.reverse();
+
+
+assert(someNumbers, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "Exercise 7")
+addToDone("Exercise 7 is correct")
+
+
+
+// Exercise 8
+// Sort the vegetables in alphabetical order. Recommend finding a way to sort the array with a built-in method
+
+vegetables.sort();
+
+assert(vegetables, ['broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 8")
+addToDone("Exercise 8 is correct.")
+
+
+
+// Exercise 9
+// Write the code necessary to sort the fruits in reverse alphabetical order
+
+fruits.sort().reverse();
+
+assert(fruits, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'], "Exercise 9")
+addToDone("Exercise 9 is correct.")
+
+
+// Exercise 10
+// Write the code necessary to produce a single array that holds all fruits then all vegetables in the order as they were sorted above.
+// Assign the result to a variable named fruitsAndVeggies.
+// *hint* the search engine search here would be "how to combine two arrays in JavaScript", for example.
+
+var fruitsAndVeggies = fruits.concat(vegetables);
+
+assert(fruitsAndVeggies, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana', 'broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 10")
+addToDone("Exercise 10 is correct")
+
+// This function generates a random number that is both positive and even
+function randomPositiveEvenNumber() {
+    var randomNumber = Math.ceil(Math.random() * 100) + 10;
+    if(randomNumber % 2 !== 0) {
+        return randomPositiveEvenNumber()
+    }
+
+    return randomNumber;
+}
+
+// this function generates a random number that is both positive and odd
+function randomPositiveOddNumber() {
+    var randomNumber = Math.ceil(Math.random() * 100) + 10;
+    if(randomNumber % 2 === 0) {
+        return randomPositiveOddNumber();
+    }
+
+    return randomNumber;
+}
+
+// this function generates a random number that is both negative and even.
+function randomNegativeEvenNumber() {
+    var randomNumber = Math.ceil(Math.random() * -100) - 10;
+    if(randomNumber % 2 === 0) {
+        return randomNumber;
+    }
+
+    return randomNegativeEvenNumber();
+}
+
+// this function generates a random number that is both negative and odd.
+function randomNegativeOddNumber() {
+    var randomNumber = Math.ceil(Math.random() * -100) - 10;
+    if(randomNumber % 2 === 0) {
+        return randomNegativeOddNumber();
+    }
+
+    return randomNumber;
+}
+
+// The next 4 lines create variables that hold these generated random numbers
+var positiveEvenNumber = randomPositiveEvenNumber()
+var positiveOddNumber = randomPositiveOddNumber();
+var negativeEvenNumber = randomNegativeEvenNumber();
+var negativeOddNumber = randomNegativeOddNumber();
+
+// Writing functions
+// See https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Functions for help with writing functions
+// Be sure to return values from your function definitions. The assert statements will call/run your function(s) for you.
+
+
+// Example function defintion:
+// Write a sayHello function that adds the string "Hello, " to the beginning and "!" to the end of any given input.
+function sayHello(name) {
+    return "Hello, " + name + "!";
+}
+
+assert(sayHello("Jane"), "Hello, Jane!");
+assert(sayHello("Pat"), "Hello, Pat!");
+assert(sayHello("Astrud"), "Hello, Astrud!");
+assert(sayHello("June"), "Hello, June!");
+assert(sayHello("World"), "Hello, World!");
+
+
+// Heres another example function definition
+// This plusTwo function takes in a variable and adds the number 2 to it.
+function plusTwo(x) {
+    return x + 2;
+}
+
+assert(plusTwo(3), 5, "3 plus 2 is five")
+assert(plusTwo(-2), 0, "-2 plus 2 is zero")
+assert(plusTwo(0), 2, "zero plus 2 is two")
+
+
+
+// Exercise 11
+// Write a function definition for a function named addOne that takes in a number and returns that number plus one
+
+function addOne(x) {
+    return x + 1;
+}
+
+
+assert(addOne(2), 3, "Exercise 11");
+assert(addOne(0), 1, "Exercise 11");
+assert(addOne(positiveEvenNumber), positiveEvenNumber + 1, "Exercise 11");
+assert(addOne(negativeOddNumber), negativeOddNumber + 1, "Exercise 11");
+addToDone("Exercise 11 is correct.")
+
+
+
+// Exercise 12
+// Write a function definition named isPositive that takes in a number and returns true or false if that number is positive.
+// Think about the definition of a number that is positive.
+// Is 5 positive? Is 0.25 positive? Is 0.00001 positive? Is -0.001 positive?
+// Is zero itself positive? What about infinity? What about negative infinity?
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators for guidance with JS comparison operators.
+
+function isPositive(x) {
+    if (x > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+assert(isPositive(0.25), true, "Exercise 12");
+assert(isPositive(0.00001), true, "Exercise 12");
+assert(isPositive(Infinity), true, "Exercise 12");
+assert(isPositive(0), false, "Exercise 12");
+assert(isPositive(-0.001), false, "Exercise 12");
+assert(isPositive(positiveOddNumber), true, "Exercise 12");
+assert(isPositive(positiveEvenNumber), true, "Exercise 12");
+assert(isPositive(negativeOddNumber), false, "Exercise 12");
+assert(isPositive(negativeEvenNumber), false, "Exercise 12");
+assert(isPositive(-Infinity), false, "Exercise 12");
+addToDone("Exercise 12 is correct.")
+
+
+// Exercise 13
+// Write a function definition named isNegative that takes in a number and returns true or False if that number is negative.
+
+function isNegative(x) {
+    if (x < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+assert(isNegative(positiveOddNumber), false, "Exercise 13");
+assert(isNegative(positiveEvenNumber), false, "Exercise 13");
+assert(isNegative(negativeOddNumber), true, "Exercise 13");
+assert(isNegative(negativeEvenNumber), true, "Exercise 13");
+addToDone("Exercise 13 is correct.")
+
+
+// Exercise 14
+// Write a function definition named isOdd that takes in a number and returns true or false if that number is odd.
+
+function isOdd(x) {
+    if (x%2!=0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+assert(isOdd(positiveOddNumber), true, "Exercise 14");
+assert(isOdd(positiveEvenNumber), false, "Exercise 14");
+assert(isOdd(negativeOddNumber), true, "Exercise 14");
+assert(isOdd(negativeEvenNumber), false, "Exercise 14");
+addToDone("Exercise 14 is correct.")
+
+// Exercise 15
+// Write a function definition named isEven that takes in a number and returns true or false if that number is even.
+
+function isEven(x) {
+    if (x%2===0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+assert(isEven(2), true, "Exercise 15");
+assert(isEven(positiveOddNumber), false, "Exercise 15");
+assert(isEven(positiveEvenNumber), true, "Exercise 15");
+assert(isEven(negativeOddNumber), false, "Exercise 15");
+assert(isEven(negativeEvenNumber), true, "Exercise 15");
+addToDone("Exercise 15 is correct.")
